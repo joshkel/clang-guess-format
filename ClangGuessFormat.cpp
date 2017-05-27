1cbb1d32f9a1181b681195fd4f62bf04196d306f
Josh Kelley <joshkel@gmail.com>
2017-05-27 17:42:02 -0400

    WIP: Reorganize into tryFormat functions

--- ClangGuessFormat.cpp
+++ ClangGuessFormat.cpp
@@ -11,45 +11,140 @@ using namespace clang;
 using namespace clang::format;
 using clang::tooling::Replacements;
 
+struct CodeFile {
+  CodeFile(std::unique_ptr<const llvm::MemoryBuffer> Code, std::string FileName)
+    : Code(std::move(Code)),
+      FileName(FileName),
+      Ranges { tooling::Range(0, this->Code->getBufferSize()) }
+  {
+  }
+
+  std::unique_ptr<const llvm::MemoryBuffer> Code;
+  std::string FileName;
+  std::vector<tooling::Range> Ranges;
+};
+
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
 
 // Source: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C
-int levenshtein(const char *s1, const char *s2, const unsigned int s1len, const unsigned int s2len) {
-    unsigned int x, y, lastdiag, olddiag;
-    unsigned int column[s1len+1];
-    for (y = 1; y <= s1len; y++)
-        column[y] = y;
-    for (x = 1; x <= s2len; x++) {
-        column[0] = x;
-        for (y = 1, lastdiag = x-1; y <= s1len; y++) {
-            olddiag = column[y];
-            column[y] = MIN3(column[y] + 1, column[y-1] + 1, lastdiag + (s1[y-1] == s2[x-1] ? 0 : 1));
-            lastdiag = olddiag;
-        }
+int levenshtein(const char *s1, const char *s2,
+    const unsigned int s1len, const unsigned int s2len)
+{
+  unsigned int column[s1len+1];
+  for (unsigned int y = 1; y <= s1len; y++) {
+    column[y] = y;
+  }
+  for (unsigned int x = 1; x <= s2len; x++) {
+    column[0] = x;
+    for (unsigned int y = 1, lastdiag = x - 1; y <= s1len; y++) {
+      unsigned int olddiag = column[y];
+      column[y] = MIN3(column[y] + 1, column[y - 1] + 1,
+          lastdiag + (s1[y - 1] == s2[x - 1] ? 0 : 1));
+      lastdiag = olddiag;
     }
-    return(column[s1len]);
+  }
+  return column[s1len];
 }
+
+int getTotalDistance(const llvm::MemoryBuffer& Code,
+                     const Replacements& FormatChanges)
+{
+  int TotalDistance = 0;
+  for (const auto& i : FormatChanges) {
+    TotalDistance += levenshtein(
+        Code.getBufferStart() + i.getOffset(), i.getReplacementText().data(),
+        i.getLength(), i.getReplacementText().size());
+  }
+  return TotalDistance;
+}
+
+template<typename T>
+std::vector<T> getValues()
+{
+  static_assert(sizeof(T) == -1, "getValues must be specialized");
+}
+
+template<>
+std::vector<bool> getValues<bool>()
+{
+  return { false, true };
+}
+
+template<typename T>
+void tryFormat(FormatStyle& Style, const std::vector<CodeFile>& CodeFiles,
+               const char *ValueName, const std::vector<T>& Values,
+               std::function<void(FormatStyle&, T)> Apply)
+{
+  for (const T& v : Values) {
+    Apply(Style, v);
+
+    int TotalDistance = 0;
+    for (const auto& CodeFile : CodeFiles) {
+      bool IncompleteFormat;
+      Replacements FormatChanges = reformat(Style, CodeFile.Code->getBuffer(),
+                                            CodeFile.Ranges, CodeFile.FileName,
+                                            &IncompleteFormat);
+      if (IncompleteFormat) {
+        errs() << CodeFile.FileName << ": " << ValueName << " " << v << "Failed\n";
+        continue;
+      }
+
+      TotalDistance += getTotalDistance(*CodeFile.Code, FormatChanges);
+    }
+
+    cout << v << ": " << TotalDistance << "\n";
+  }
+}
+
+template<typename T>
+void tryFormat(FormatStyle& Style, const std::vector<CodeFile>& CodeFiles,
+               const char *ValueName, T FormatStyle::*Value)
+{
+  tryFormat(Style, CodeFiles, ValueName, getValues<T>(),
+            [Value](FormatStyle& Style, T v) { (Style.*Value) = v; });
+}
+
+#define TRY(x)
 
 int main(int argc, char **argv)
 {
-  const std::string FileName(argv[1]);
-  const char * const try_formats[] = { "LLVM", "Google", "Chromium", "Mozilla", "WebKit" };
-
-  for (const char * format : try_formats) {
-    ErrorOr<std::unique_ptr<MemoryBuffer>> CodeOrErr = MemoryBuffer::getFileOrSTDIN(FileName);
+  std::vector<CodeFile> CodeFiles;
+  for (int i = 1; i < argc; i++) {
+    const std::string FileName = argv[i];
+    ErrorOr<std::unique_ptr<MemoryBuffer>> CodeOrErr =
+        MemoryBuffer::getFileOrSTDIN(FileName);
     if (std::error_code EC = CodeOrErr.getError()) {
-      errs() << EC.message() << "\n";
+      errs() << "Failed to read " << FileName << ": " << EC.message() << "\n";
       return 1;
     }
-    std::unique_ptr<llvm::MemoryBuffer> Code = std::move(CodeOrErr.get());
-    if (Code->getBufferSize() == 0)
-      return false; // Empty files are formatted correctly.
-    std::vector<tooling::Range> Ranges = { tooling::Range(0, Code->getBufferSize()) };
-    FormatStyle Style;
-    if (!clang::format::getPredefinedStyle(format, FormatStyle::LK_Cpp, &Style)) {
-      cout << "Failed to get style " << format << "\n";
+    std::unique_ptr<const llvm::MemoryBuffer> Code = std::move(CodeOrErr.get());
+    if (Code->getBufferSize() == 0) {
+      // Empty files are formatted correctly.
+      errs() << FileName << " is empty, skipping.\n";
       continue;
     }
+    CodeFiles.push_back({ std::move(Code), FileName });
+  }
+
+  outs() << "# Total edit distance (lower is better)\n";
+
+  FormatStyle Style;
+
+  tryFormat<const char*>(
+      Style, CodeFiles, "BasedOnStyle",
+      { "LLVM", "Google", "Chromium", "Mozilla", "WebKit" },
+      [](FormatStyle& Style, const char *StyleName) {
+        if (!clang::format::getPredefinedStyle(StyleName, FormatStyle::LK_Cpp, &Style)) {
+          errs() << "Failed to get style " << StyleName << "\n";
+          exit(1);
+        }
+      });
+  /*
+  FormatStyle Style;
+  clang::format::getPredefinedStyle("Mozilla", FormatStyle::LK_Cpp, &Style);
+  for (int i = 0; i <= 1; i++) {
+    Style.AllowShortIfStatementsOnASingleLine = i;
+
     bool IncompleteFormat;
     Replacements FormatChanges = reformat(Style, Code->getBuffer(), Ranges, FileName, &IncompleteFormat);
     if (IncompleteFormat) {
@@ -57,12 +152,10 @@ int main(int argc, char **argv)
       return 1;
     }
 
-    cout << format << ": " << FormatChanges.size() << endl;
+    cout << i << ": " << FormatChanges.size() << endl;
 
-    int TotalDistance = 0;
-    for (const auto& i : FormatChanges) {
-      TotalDistance += levenshtein(Code->getBufferStart() + i.getOffset(), i.getReplacementText().data(), i.getLength(), i.getReplacementText().size());
-    }
+    int TotalDistance = getTotalDistance(*Code, FormatChanges);
     cout << "    " << TotalDistance << endl;
   }
+  */
 }
